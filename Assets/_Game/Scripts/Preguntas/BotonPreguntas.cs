dde3603777eb1f261288afff535b8d700570c75d
Morion <adrian.martinez@udea.edu.co>
2021-04-02 13:37:02 -0500

    Preguntas

---
+++ Assets/_Game/Scripts/Preguntas/BotonPreguntas.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonPreguntas : MonoBehaviour
+{
+	public int opcion;
+	public Text txtBoton;
+
+	public void Inicializar(string respuesta, int o)
+	{
+		opcion = o;
+		txtBoton.text = respuesta;
+	}
+    public void Verificar()
+	{
+		GestorPreguntas.singleton.Verificar(opcion);
+	}
+}
