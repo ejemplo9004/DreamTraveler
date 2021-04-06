c3315838d9907200f17a0276d71da1fac1dc4265
Morion <adrian.martinez@udea.edu.co>
2021-04-05 19:33:34 -0500

    Avancces del juego de palabras

---
+++ Assets/_Game/Scripts/ui/BotonPalabrasSilaba.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonPalabrasSilaba : MonoBehaviour
+{
+	public string letras;
+	public Text txt;
+
+	private void Start()
+	{
+		txt.text = letras;
+		BotoneraPalabras.singleton.botones.Add(this);
+	}
+
+	public void Agregar()
+	{
+		BotoneraPalabras.singleton.AgregarSilava(letras);
+		Desaparecer();
+	}
+
+	public void Desaparecer()
+	{
+		LeanTween.scale(gameObject, Vector3.zero, 0.2f);
+	}
+	public void Aparecer()
+	{
+		LeanTween.scale(gameObject, Vector3.one, 0.2f);
+	}
+}
---
+++ Assets/_Game/Scripts/ui/PalabrasOcultas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PalabrasOcultas : MonoBehaviour
+{
+    public Text txt;
+    public string palabra;
+	
+    public void Inicializar(string p)
+	{
+        palabra = p;
+		txt.text = "";
+		for (int i = 0; i < p.Length; i++)
+		{
+			txt.text += "■";
+		}
+	}
+
+	public bool Comparar(string p)
+	{
+		if (p.Equals(palabra))
+		{
+			txt.text = palabra;
+			return true;
+		}
+		return false;
+	}
+}
