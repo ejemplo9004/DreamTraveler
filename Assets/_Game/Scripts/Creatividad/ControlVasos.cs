94f5c34db415e7366f949f3544261c45b1b7b69e
Morion <adrian.martinez@udea.edu.co>
2021-04-17 00:48:04 -0500

    Inicio del juego del agua

---
+++ Assets/_Game/Scripts/Creatividad/ControlVasos.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlVasos : MonoBehaviour
+{
+    public Button btnVotear;
+    public Button btnEnderezar;
+    public bool arriba;
+    public int posicionX;
+    public Transform tSeleccion;
+    public bool inicioMovimiento;
+    public int posVasoSeleccionado;
+    public Vaso[] vasos;
+
+    public static ControlVasos singleton;
+
+    private void Awake()
+    {
+        singleton = this;
+    }
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Subir()
+    {
+
+		if (Vaso.vasoActivo != null)
+        {
+            inicioMovimiento = true;
+            Vaso.vasoActivo.Subir();
+            arriba = Vaso.vasoActivo.arriba;
+		}
+    }
+
+    public void Bajar()
+    {
+        if (Vaso.vasoActivo != null)
+        {
+            inicioMovimiento = true;
+            Vaso.vasoActivo.Bajar();
+            arriba = Vaso.vasoActivo.arriba;
+        }
+    }
+
+    public void Izquierda()
+    {
+        if (Vaso.vasoActivo != null)
+        {
+            inicioMovimiento = true;
+            Vaso.vasoActivo.Izquierda();
+            posicionX = Mathf.RoundToInt(Vaso.vasoActivo.px);
+        }
+    }
+    public void Derecha()
+    {
+        if (Vaso.vasoActivo != null)
+        {
+            inicioMovimiento = true;
+            Vaso.vasoActivo.Derecha();
+            posicionX = Mathf.RoundToInt(Vaso.vasoActivo.px);
+        }
+    }
+    public void Voltear()
+    {
+        if (Vaso.vasoActivo != null && !Vaso.vasoActivo.volteado)
+        {
+			if (Vaso.vasoActivo.lleno)
+			{
+				if (!vasos[posicionX/2].lleno)
+				{
+                    vasos[posicionX / 2].animator.SetTrigger("activar");
+                }
+			}
+            inicioMovimiento = true;
+            Vaso.vasoActivo.Voltear();
+            btnVotear.interactable = false;
+            btnEnderezar.interactable = true;
+        }
+    }
+    public void Enderezar()
+    {
+        if (Vaso.vasoActivo != null && Vaso.vasoActivo.volteado)
+        {
+            inicioMovimiento = true;
+            Vaso.vasoActivo.Enderezar();
+            btnVotear.interactable = true;
+            btnEnderezar.interactable = false;
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Creatividad/Vaso.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vaso : MonoBehaviour
+{
+    public bool lleno;
+    public Animator animator;
+    public float pArriba;
+    public float pAbajo;
+    public bool arriba = false;
+    public float px;
+    public bool volteado;
+
+    public static Vaso vasoActivo;
+
+    float pActual;
+    float pXActual;
+    void Start()
+    {
+        pActual = pAbajo;
+        pXActual = (float)(Mathf.RoundToInt(transform.position.x));
+        px = pXActual;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pActual = Mathf.Lerp(pActual, (arriba ? pArriba : pAbajo), Time.deltaTime*5);
+        pXActual = Mathf.Lerp(pXActual, px, Time.deltaTime*5);
+        transform.position = new Vector3(pXActual, pActual, 0);
+    }
+
+	private void OnMouseDown()
+	{
+		if (!ControlVasos.singleton.inicioMovimiento)
+		{
+            vasoActivo = this;
+            ControlVasos.singleton.posicionX = Mathf.RoundToInt(px);
+            ControlVasos.singleton.tSeleccion.position = new Vector3(transform.position.x, ControlVasos.singleton.tSeleccion.position.y, 0);
+            ControlVasos.singleton.posVasoSeleccionado = Mathf.RoundToInt(px);
+        }
+
+    }
+
+	public void Subir()
+	{
+        arriba = true;
+	}
+
+    public void Bajar()
+	{
+        arriba = false;
+	}
+
+    public void Izquierda()
+    {
+        px --;
+    }
+    public void Derecha()
+    {
+        px++;
+    }
+    public void Voltear()
+    {
+        if (!volteado)
+        {
+            animator.SetTrigger("activar");
+            volteado = true;
+            lleno = false;
+        }
+    }
+    public void Enderezar()
+    {
+        if (volteado)
+        {
+            animator.SetTrigger("activar");
+            volteado = false;
+        }
+    }
+
+}
