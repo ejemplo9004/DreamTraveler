c4cf50e2334f4df8edb2f4db564c1fc75486a8e0
Morion <adrian.martinez@udea.edu.co>
2021-04-17 18:34:42 -0500

    Avances Amelia

---
+++ Assets/_Game/Scripts/Comunicacion/ControlComunicacion.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlComunicacion : MonoBehaviour
+{
+    public Dialogo[] dialogos;
+    public Animator animP0;
+    public Animator animP1;
+    public Text txtNormal;
+    public Button btns;
+    public Image imCara;
+    public GameObject gmOpciones;
+
+    public int indice;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MostrarOpcion(Dialogo d)
+	{
+        imCara.sprite = d.imagen;
+
+		if (d.multiOpciones)
+		{
+            txtNormal.gameObject.SetActive(false);
+            gmOpciones.SetActive(true);
+			for (int i = 0; i < d.opciones.Length; i++)
+			{
+
+			}
+        }
+        else
+		{
+            txtNormal.gameObject.SetActive(true);
+            gmOpciones.SetActive(false);
+        }
+	}
+}
+
+[System.Serializable]
+public class Dialogo
+{
+    public string frase;
+    public Sprite imagen;
+    public int puntos;
+    public int siguiente;
+    public bool multiOpciones;
+    public string[] opciones;
+}
